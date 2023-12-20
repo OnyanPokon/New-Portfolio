f02d607ef32ce0ae242e8f1c93aa81a4ae12285b
OnyanPokon <mohamadrafiqd@gmail.com>
2023-12-20 16:28:41 +0800

    integrate linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,13 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true, node: true},
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'airbnb',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'linebreak-style': 'off',
+    'import/no-extraneous-dependencies' : 'off'
   },
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,90 +1,50 @@
+import React from 'react';
+import Plus from '../components/Plus';
+import react from '../assets/react.svg';
+
 function Homepage() {
   return (
-    <>
-      <div className="min-h-screen flex items-center px-4">
-        <section className="max-w-screen-xl mx-auto flex flex-col justify-center w-full">
-          <div className="flex flex-col items-center gap-4">
-            <div className="flex gap-8">
-              <img
-                className="w-16 h-16 rounded-full object-cover"
-                src="/rafiq.jpg"
-                alt="Rounded avatar"
-              />
-              <img
-                className="w-16 h-16 rounded-full object-cover"
-                src="/rafiq.jpg"
-                alt="Rounded avatar"
-              />
-            </div>
-            <div className="font-incon text-center text-xs uppercase">
-              <p>
-                Welcome to my portfolio website <br />{" "}
-                {`[use dekstop for better experience]`}
-              </p>
-            </div>
+    <div className="min-h-screen flex items-center px-4">
+      <section className="max-w-screen-xl mx-auto flex flex-col justify-center w-full">
+        <div className="flex flex-col items-center gap-4">
+          <div className="flex gap-8">
+            <img
+              className="w-16 h-16 rounded-full ring-2 ring-gray "
+              src="/rafiq.jpg"
+              alt="Rounded avatar"
+            />
+            <img
+              className="w-16 h-16 rounded-full animate-spin"
+              src={react}
+              alt="Rounded avatar"
+            />
           </div>
-          <div className="flex lg:justify-between justify-center items-center">
-            <div className="hidden lg:block">
-              <svg
-                width="50"
-                height="50"
-                viewBox="0 0 100 100"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect width="100" height="100" fill="white" />
-                <line
-                  x1="51"
-                  y1="11"
-                  x2="51"
-                  y2="90"
-                  stroke="black"
-                  strokeWidth="2"
-                />
-                <line
-                  x1="90"
-                  y1="51"
-                  x2="11"
-                  y2="51"
-                  stroke="black"
-                  strokeWidth="2"
-                />
-              </svg>
-            </div>
-            <div className="text-center uppercase">
-              <h2 className="font-dharma font-semibold lg:text-[25rem]  text-[10rem] leading-none">hello !</h2>
-            </div>
-            <div className="hidden lg:block">
-              <svg
-                width="50"
-                height="50"
-                viewBox="0 0 100 100"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect width="100" height="100" fill="white" />
-                <line
-                  x1="51"
-                  y1="11"
-                  x2="51"
-                  y2="90"
-                  stroke="black"
-                  strokeWidth="2"
-                />
-                <line
-                  x1="90"
-                  y1="51"
-                  x2="11"
-                  y2="51"
-                  stroke="black"
-                  strokeWidth="2"
-                />
-              </svg>
-            </div>
+          <div className="font-incon text-center text-xs uppercase">
+            <p>
+              Welcome to my portfolio website
+              {' '}
+              <br />
+              {' '}
+              [use dekstop for better
+              experience]
+            </p>
           </div>
-        </section>
-      </div>
-    </>
+        </div>
+        <div className="flex lg:justify-between justify-center items-center">
+          <div className="hidden lg:block">
+            <Plus size="50" />
+          </div>
+          <div className="text-center uppercase">
+            <h2 className="font-dharma font-semibold lg:text-[25rem]  text-[10rem] leading-none cursor-default">
+              hello !
+            </h2>
+          </div>
+          <div className="hidden lg:block">
+            <Plus size="50" />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
